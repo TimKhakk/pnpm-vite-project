c1b342609e7eed6e94913aff062f25359992fc43
Timur Khakkiev <khakkievtimur@yandex.ru>
2024-09-26 13:53:08 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { SelectScrollable } from '@/components/ui/selectScrollable'
+import './App.css'
+
+function App() {
+
+  return (
+    <main className='container flex mx-auto'>
+      <SelectScrollable />
+    </main>
+  )
+}
+
+export default App
